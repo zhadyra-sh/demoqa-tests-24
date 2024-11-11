697b8ed0f29d091302250118de3fb3e7d400cdd6
Zhadyra Shynybayeva <shzhadyra96@gmail.com>
2024-11-11 16:57:01 +0500

    added remote demoqa test

---
+++ src/test/java/tests/simple/RegistrationRemoteTests.java
@@ -0,0 +1,60 @@
+package tests.simple;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class RegistrationRemoteTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
+    }
+
+
+    @Test
+    @Tag("demoqa")
+    void successfulRegistrationTest() {
+        step("Open from", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
+        });
+        step("Fill form", () -> {
+            $("#firstName").setValue("Test");
+            $("#lastName").setValue("Testing");
+            $("#userEmail").setValue("testing@gmail.ru");
+            $("#genterWrapper").$(byText("Other")).click();
+            $("#userNumber").setValue("1234567890");
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("July");
+            $(".react-datepicker__year-select").selectOption("2008");
+            $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+            $("#subjectsInput").setValue("Math").pressEnter();
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+            $("#uploadPicture").uploadFromClasspath("1.png");
+            $("#currentAddress").setValue("Some address 1");
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+            $("#submit").click();
+        });
+        step("Verify results", () -> {
+            $(".modal-dialog").should(appear);
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").shouldHave(text("Test"), text("Testing"),
+                    text("testing@gmail.ru"), text("1234567890"));
+        });
+
+    }
+}
---
+++ src/test/java/tests/simple/TestBase.java
@@ -0,0 +1,17 @@
+package tests.simple;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+//        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+//        Configuration.holdBrowserOpen = true;
+    }
+
+}
