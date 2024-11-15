e9912f9a1a978a651852c8370319d366cadcf8e4
Zhadyra Shynybayeva <shzhadyra96@gmail.com>
2024-11-15 15:08:07 +0500

    added remote demoqa test

--- src/test/java/tests/simple/RegistrationRemoteTests.java
+++ src/test/java/tests/simple/RegistrationRemoteTests.java
@@ -16,7 +16,10 @@ public class RegistrationRemoteTests {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+      //  Configuration.browser ="chrome";
         Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
     }
 
 
