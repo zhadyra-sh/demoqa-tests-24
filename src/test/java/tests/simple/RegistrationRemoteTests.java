82e9f6d4cea47bdc7c72449267e79b8559685499
Zhadyra Shynybayeva <shzhadyra96@gmail.com>
2024-11-16 22:30:46 +0500

    added video attach

--- src/test/java/tests/simple/RegistrationRemoteTests.java
+++ src/test/java/tests/simple/RegistrationRemoteTests.java
@@ -6,6 +6,9 @@ import helpers.Attach;
 import io.qameta.allure.Owner;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
@@ -21,6 +24,14 @@ public class RegistrationRemoteTests {
         //  Configuration.browser ="chrome";
         Configuration.timeout = 10000;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of( //video
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
     }
