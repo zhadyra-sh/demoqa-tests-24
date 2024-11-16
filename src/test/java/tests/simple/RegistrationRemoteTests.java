3789aac51a31bab5ddb468eea7f64db542015755
Zhadyra Shynybayeva <shzhadyra96@gmail.com>
2024-11-16 19:57:28 +0500

    added void addAttachments()

--- src/test/java/tests/simple/RegistrationRemoteTests.java
+++ src/test/java/tests/simple/RegistrationRemoteTests.java
@@ -3,12 +3,10 @@ package tests.simple;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.Owner;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
@@ -26,6 +24,11 @@ public class RegistrationRemoteTests {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
     }
 
     @DisplayName("Проверка формы регистрации")
